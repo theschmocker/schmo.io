b1b78f59c70a882c7cc2ad59f207b5cf16c22bfa
theschmocker <jacobcs1996@gmail.com>
2018-03-19 22:15:27 -0500

    minor transition changes

--- src/utils/getTransitionStyle.js
+++ src/utils/getTransitionStyle.js
@@ -1,20 +1,23 @@
 const getTransitionStyles = timeout => {
-  return {
-    entering: {
-      opacity: 0,
-    },
-    entered: {
-      transition: `opacity ${timeout}ms ease-in-out`,
-      opacity: 1,
-    },
-    exiting: {
-      transition: `opacity ${timeout}ms ease-in-out`,
-      opacity: 0,
-    },
-  }
+    return {
+        entering: {
+            opacity: 0,
+            transform: 'translateY(0.5rem)'
+        },
+        entered: {
+            transition: `opacity ${timeout}ms ease, transform ${timeout}ms ease`,
+            opacity: 1,
+            transform: 'translateY(0px)'
+        },
+        exiting: {
+            transition: `opacity ${timeout}ms ease, transform ${timeout}ms ease`,
+            opacity: 0,
+            transform: 'translateY(0.5rem)'
+        },
+    }
 }
 
 const getTransitionStyle = ({ timeout, status }) =>
-  getTransitionStyles(timeout)[status]
+    getTransitionStyles(timeout)[status]
 
 export default getTransitionStyle
