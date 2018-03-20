f0213e52cac4bcc273b16135331bbea7a15dbc9d
theschmocker <jacobcs1996@gmail.com>
2018-03-19 22:12:43 -0500

    add project page stub

---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Section from '../components/StyledSection';
+
+const ProjectPage = ({ transition }) => (
+  <Section style={transition && transition.style}>
+    <h2>Project</h2>
+    <p>This page is a stub. Will implement project listing.</p>
+  </Section>
+)
+
+export default ProjectPage
