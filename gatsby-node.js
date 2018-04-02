63b2672950fa891f5971922dae8a24b1dc1b884e
theschmocker <jacobcs1996@gmail.com>
2018-04-01 20:50:27 -0500

    change how post slugs are made

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,3 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
- // You can delete this file if you're not using it
-
-// this is all gibberish and I have no idea what's going on 
 const { createFilePath } = require('gatsby-source-filesystem');
 const path = require('path');
 const slugify = require('slugify');
@@ -14,17 +5,16 @@ const slugify = require('slugify');
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
     const { createNodeField } = boundActionCreators;
     if (node.internal.type === 'MarkdownRemark') {
-        //const slug = createFilePath({
-        //    node,
-        //    getNode,
-        //    basePath: 'posts',
-        //});
-        const slug = slugify(node.frontmatter.title, { lower: true });
-        console.log('\n' + slug + '\n');
+        const slug = createFilePath({
+            node,
+            getNode,
+            basePath: 'pages',
+        });
+
         createNodeField({
             node,
             name: 'slug',
-            value: `/blog/${slug}/`,
+            value: slug,
         });
     }
 }
@@ -48,7 +38,7 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
             result.data.allMarkdownRemark.edges.forEach(({ node }) => {
                 createPage({
                     path: node.fields.slug,
-                    component: path.resolve('./src/components/BlogPost/index.js'),
+                    component: path.resolve('./src/templates/BlogPost/index.js'),
                     context: {
                         slug: node.fields.slug,
                     }
