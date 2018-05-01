9f4bacaa92808abef0d6dbced439052065ec00da
theschmocker <jacobcs1996@gmail.com>
2018-04-30 20:22:28 -0500

    add react-burger-menu. still need to work out some kinks

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import styled, { keyframes } from 'styled-components';
+import Menu from 'react-burger-menu/lib/menus/slide';
 
+//const Navigation = () => (
+//    <Nav>
+//        <ul>
+//            <li><NavLink to="/about">About</NavLink></li>
+//            <li><NavLink to="/projects">Projects</NavLink></li>
+//            <li><NavLink to="/blog">Blog</NavLink></li>
+//        </ul>
+//    </Nav>
+//)
 const Navigation = () => (
-    <Nav>
-        <ul>
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/projects">Projects</NavLink></li>
-            <li><NavLink to="/blog">Blog</NavLink></li>
-        </ul>
-    </Nav>
+    <Menu customCrossIcon={ 
+    <svg width="100px" height="100px" viewPort="0 0 30 30" version="1.1"
+        xmlns="http://www.w3.org/2000/svg">
+        <line x1="1" y1="11" 
+            x2="11" y2="1" 
+            stroke="#ddd" 
+            strokeWidth="2"/>
+        <line x1="1" y1="1" 
+            x2="11" y2="11" 
+            stroke="#ddd" 
+            strokeWidth="2"/>
+    </svg>
+    } right>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+        <NavLink to="/blog">Blog</NavLink>
+    </Menu>
 )
 
 const NavLink = ({ to, children }) => (
     <Link 
+        exact
         to={to}
         activeStyle={{
             borderBottom: '2px solid #ccc',
