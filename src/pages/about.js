b6f365415d8078508c141ed6e2df6dc4bb0aeecf
theschmocker <jacobcs1996@gmail.com>
2018-05-04 13:33:57 -0500

    styling changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,7 @@ const AboutPage = () => (
             <p>
                 Ugh four dollar toast mustache celiac poutine dreamcatcher. Truffaut irony man bun tote bag. Chicharrones stumptown you probably haven't heard of them, banh mi craft beer marfa roof party quinoa yr neutra snackwave food truck. Brunch schlitz banjo chillwave, cloud bread small batch hashtag pop-up affogato keffiyeh meh unicorn hexagon.
             </p>
+            <p>Next level flexitarian woke unicorn, VHS cliche humblebrag yr schlitz brunch man braid tote bag food truck cray. Marfa twee subway tile next level pug keytar direct trade jean shorts synth plaid cardigan sriracha actually put a bird on it bespoke. Mixtape PBR&amp;B celiac beard palo santo marfa glossier before they sold out post-ironic. </p>
         </AboutSection>
         <AboutSection>
             <H2>Skills</H2>
@@ -26,6 +27,8 @@ const AboutPage = () => (
 
 export default AboutPage
 
+// Styled Components
+
 const AboutWrapper = styled.article`
     margin: 0;
     padding: 1.75rem;
@@ -41,7 +44,7 @@ const AboutSection = styled.section`
 
 const H2 = styled.h2`
     position: relative;
-    margin: 0 0 1rem 0;
+    margin: 0 0 1.5rem 0;
     text-transform: uppercase;
     width: intrinsic;
     width: max-content;
@@ -51,7 +54,7 @@ const H2 = styled.h2`
         position: absolute;
         display: block;
         bottom: -6px;
-        width: 50%;
+        width: 55%;
         height: 3px;
         background: #bd1d00;
     }
