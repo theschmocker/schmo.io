db494503da014d247f7b23096552a211a6be5b91
theschmocker <jacobcs1996@gmail.com>
2018-05-04 13:28:34 -0500

    styling and placeholder content

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,32 +3,56 @@ import Link from 'gatsby-link'
 import styled from 'styled-components';
 
 const AboutPage = () => (
-  <AboutWrapper>
-    <H2>About Me</H2>
-    <H2>Skills</H2>
-    <H2>About this Site</H2>
+    <AboutWrapper>
+        <AboutSection>
+            <H2>About Me</H2>
+            <p>
+                Ugh four dollar toast mustache celiac poutine dreamcatcher. Truffaut irony man bun tote bag. Chicharrones stumptown you probably haven't heard of them, banh mi craft beer marfa roof party quinoa yr neutra snackwave food truck. Brunch schlitz banjo chillwave, cloud bread small batch hashtag pop-up affogato keffiyeh meh unicorn hexagon.
+            </p>
+        </AboutSection>
+        <AboutSection>
+            <H2>Skills</H2>
+            <p>
+            </p>
+        </AboutSection>
+        <AboutSection>
+            <H2>About this Site</H2>
+            <p>
+            </p>
+        </AboutSection>
     
   </AboutWrapper>
 )
 
 export default AboutPage
 
-const AboutWrapper = styled.section`
+const AboutWrapper = styled.article`
+    margin: 0;
+    padding: 1.75rem;
+    background: #444;
     font-family: 'Open Sans', sans-serif;
+    color: #ddd;
+    min-height: 100vh;
+`;
+
+const AboutSection = styled.section`
+
 `;
 
 const H2 = styled.h2`
     position: relative;
+    margin: 0 0 1rem 0;
     text-transform: uppercase;
     width: intrinsic;
     width: max-content;
+    color: #eee;
     &:after {
         content: "";
         position: absolute;
         display: block;
         bottom: -6px;
         width: 50%;
-        height: 6px;
+        height: 3px;
         background: #bd1d00;
     }
 `;
