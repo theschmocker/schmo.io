109ede38b2b2cb81c70eb82ade49afb89fde9aa2
theschmocker <jacobcs1996@gmail.com>
2018-07-08 00:02:15 -0500

    remove old transition prop

--- src/templates/BlogPost/index.js
+++ src/templates/BlogPost/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Section from '../../components/StyledSection';
 
-const Post = ({ data, transition }) => (
-    <Section style={transition && transition.style}>
+const Post = ({ data }) => (
+    <Section>
         <h2>{data.markdownRemark.frontmatter.title}</h2>
         <p>{data.markdownRemark.frontmatter.date}</p>
         <p><em>{data.markdownRemark.frontmatter.description}</em></p>
