34251f671cbc9f476a747e4e6bcea691a72a7d05
theschmocker <jacobcs1996@gmail.com>
2018-08-09 10:52:12 -0500

    remove old transition prop

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,8 +4,8 @@ import Link from 'gatsby-link'
 import Section from '../components/StyledSection';
 import PostListing from '../components/PostListing';
 
-const BlogPage = ({ transition, data }) => (
-    <Section style={transition && transition.style}> 
+const BlogPage = ({ data }) => (
+    <Section> 
         {
             data.allMarkdownRemark.edges.map(({ node }) => (
                 <PostListing key={node.id} post={node}/>
@@ -18,7 +18,10 @@ export default BlogPage
 
 export const query = graphql`
     query PostQuery {
-        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+        allMarkdownRemark(
+            sort: { fields: [frontmatter___date], order: DESC }
+            filter: { fields: { type: { eq: "blog" } } }
+        ) {
             edges {
                 node {
                     id
