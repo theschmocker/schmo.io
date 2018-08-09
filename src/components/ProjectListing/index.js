34df2688dc24aab0a983cd024aa350108e9b114b
theschmocker <jacobcs1996@gmail.com>
2018-08-09 13:39:46 -0500

    pull ProjectListing out of projects page file

---
+++ src/components/ProjectListing/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'gatsby-link'
+import styled from 'styled-components';
+
+const ProjectListing = ({ project, className }) => (
+    <div className={className}>
+        <Link to={project.fields.slug}><h3>{project.frontmatter.name}</h3></Link>
+        <p>{project.frontmatter.description}</p>
+    </div>
+)
+
+export default styled(ProjectListing)`
+    a {
+        opacity: 0.9;
+        &:visited {
+            color: #eee;
+        }
+        &:hover {
+            color: #fff;
+        }
+    }
+
+    p {
+        opacity: 0.7;
+    }
+`;
