6b215e4fbafa9ec5a5b0ec99d76a2134e189f44a
theschmocker <jacobcs1996@gmail.com>
2018-08-09 10:53:18 -0500

    write graphql query to get a list of projects. display them

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,11 +2,40 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Section from '../components/StyledSection';
 
-const ProjectPage = ({ transition }) => (
-  <Section style={transition && transition.style}>
-    <h2>Project</h2>
-    <p>This page is a stub. Will implement project listing.</p>
+const ProjectListing = ({ project }) => (
+    <div>
+        <Link to={project.fields.slug}><h3>{project.frontmatter.name}</h3></Link>
+        <p>{project.frontmatter.description}</p>
+    </div>
+)
+
+const ProjectPage = ({ data }) => (
+  <Section>
+    <h2>Projects</h2>
+    {data.allMarkdownRemark.edges.map(({ node }) => <ProjectListing project={node} />)}
   </Section>
 )
 
 export default ProjectPage
+
+export const query = graphql`
+    query ProjectQuery {
+        allMarkdownRemark(
+            filter: { 
+                fields: { type: {eq: "projects"} } 
+            }
+        ) {
+            edges {
+                node {
+                    fields {
+                        slug
+                    }
+                    frontmatter {
+                        name
+                        description
+                    }
+                }
+            }
+        }
+    }
+`;
