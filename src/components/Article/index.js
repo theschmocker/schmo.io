a91766634047a73eac208bad7eb9385409f97b7e
theschmocker <jacobcs1996@gmail.com>
2018-08-09 13:39:15 -0500

    create generic, styled article component

---
+++ src/components/Article/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Article = styled.article`
+    font-family: 'Roboto', sans-serif;
+    background: #333;
+    color: #ccc;
+    min-height: calc(100vh - 100px);
+`;
+
+export default Article;
