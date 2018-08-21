7a81edd7bbaaaf919d6c8554483d21bc06ef2a72
theschmocker <jacobcs1996@gmail.com>
2018-08-20 20:50:09 -0500

    add link to github for more projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import ContentHeading from '../components/ContentHeading';
 import Article from '../components/Article';
 import ProjectListing from '../components/ProjectListing';
+import StyledAnchor from '../components/StyledAnchor';
 
 
 const ProjectPage = ({ data }) => (
@@ -11,8 +12,9 @@ const ProjectPage = ({ data }) => (
         <StyledProjectsList>
             <ContentHeading>Projects</ContentHeading>
             {data.allMarkdownRemark.edges.map(({ node }) => <ProjectListing project={node} />)}
+        <OtherProjects>You can check out some of my other projects on <StyledAnchor href="https://github.com/theschmocker">Github</StyledAnchor></OtherProjects>
         </StyledProjectsList>
-  </Article>
+    </Article>
 )
 
 const StyledProjectsList = styled.article`
@@ -20,6 +22,14 @@ const StyledProjectsList = styled.article`
     margin: 0 auto;
     max-width: 600px;
     color: #eee;
+`;
+
+const OtherProjects = styled.p`
+    margin: 1.6rem 0;
+    padding: 1.5rem 0;
+    border-top: 1px solid #777;
+    font-size: 1.1rem;
+    opacity: 0.7;
 `;
 
 export default ProjectPage
